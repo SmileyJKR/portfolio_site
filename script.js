76503c49e316bd07d1fb57c259dff0e8bf88a7be
Smiley <jrmcghee@asu.edu>
2026-07-01 23:08:44 +0900

    WinXP simple setup **need to change background img and so on later**

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function openWindow(windowId){
+  const win = document.getElementById(windowId);
+  if(win) {
+    win.style.display = 'block';
+  }
+}
 
+function closeWindow(windowId){
+  const win = document.getElementById(windowId);
+  if(win) {
+    win.style.display = 'none';
+  }
+}
