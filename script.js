0d576a9925de992dbfecd8ddaba1396b672c1070
SmileyJKR <JRMcGhee2004@hotmail.com>
2026-07-08 14:03:24 +0900

    new Icons and start button

--- script.js
+++ script.js
@@ -11,3 +11,36 @@ function closeWindow(windowId){
     win.style.display = 'none';
   }
 }
+
+function toggleStartMenu() {
+  if(event){
+    event.stopPropagation();
+  }
+  
+  const startButton = document.querySelector('.start-button');
+  const startMenu = document.getElementById('start-menu');
+
+  startButton.classList.toggle('is-active');
+
+  //showing / hiding the start menu
+  if(startMenu){
+    if(startButton.classList.contains('is-active')){
+      startMenu.style.display = 'block';
+    } else {
+      startMenu.style.display = 'none';
+    }
+  }
+}
+
+document.addEventListener('click', function(event) {
+  const startButton = document.querySelector('.start-button');
+  const startMenu = document.getElementById('start-menu');
+
+  if(!startButton.contains(event.target) && (!startMenu || !startMenu.contains(event.target))) {
+    startButton.classList.remove('is-active');
+  
+    if(startMenu){
+      startMenu.style.display = 'none';
+    }
+  }
+});
