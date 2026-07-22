5a8124ff00714850e10abefc0501c552c7262f1e
SmileyJKR <JRMcGhee2004@hotmail.com>
2026-07-22 17:43:19 +0900

    added windows for most

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const windowState = {} // Object to store the state of each window  
+let zCounter = 20; //manages the z index of windows
+
+document.querySelectorAll('.window').forEach(win => {
+  windowState[win.id] = {
+    isMaximized: false,
+    isMinimized: false,
+  }
+
+  win.querySelector('.title-bar').addEventListener('mousedown', function(event){
+    dragWindow(event, win.id);
+  })
+});
+
+function dragWindow(event, windowId) {
+  const win = document.getElementById(windowId);
+  let shiftX = event.clientX - win.getBoundingClientRect().left;
+  let shiftY = event.clientY - win.getBoundingClientRect().top;
+
+  win.style.zIndex = ++zCounter; //bring the window to the front
+  win.style.left = event.clientX - shiftX + 'px';
+  win.style.top = event.clientY - shiftY + 'px';
+
+  function moveAt(pageX, pageY) {
+    win.style.left = pageX - shiftX + 'px';
+    win.style.top = pageY - shiftY + 'px';
+  }
+  
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  //move the window on mousemove
+  document.addEventListener('mousemove', onMouseMove);
+
+  //drop the window, remove unneeded handlers
+  document.onmouseup = function() {
+    document.removeEventListener('mousemove', onMouseMove);
+    document.onmouseup = null;
+  };
+}
+
+
 function openWindow(windowId){
   const win = document.getElementById(windowId);
   if(win) {
@@ -11,6 +54,35 @@ function closeWindow(windowId){
     win.style.display = 'none';
   }
 }
+
+function maximizeWindow(windowId){
+  const win = document.getElementById(windowId);
+  const state = windowState[windowId];
+  if(!state.isMaximized){
+    state.prevWidth = win.style.width;
+    state.prevHeight = win.style.height;
+    state.prevLeft = win.style.left;
+    state.prevTop = win.style.top;
+
+    win.style.width = '100%';
+    win.style.height = '100%';
+    win.style.left = '0';
+    win.style.top = '0';
+    state.isMaximized = true;
+  } else { //put everything back to the way it was before it was maximized
+    win.style.width = state.prevWidth;
+    win.style.height = state.prevHeight;
+    win.style.left = state.prevLeft;
+    win.style.top = state.prevTop;
+    state.isMaximized = false;
+  }
+}
+
+// function minimizeWindow(windowId){
+//   const win = document.getElementById(windowId);
+//   //implement later once taskbar is implemented
+//   break
+// }
 
 function toggleStartMenu() {
   if(event){
